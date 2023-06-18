eda49cb30382f49a60f011cd5056d16b4b26e260
Anna Torgunakova <annatorgun@mail.ru>
2023-06-18 20:24:04 +0700

    added practice period controller

--- services/practice-service/Program.cs
+++ services/practice-service/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using practice_service;
+using practice_service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IPracticePeriodService, PracticePeriodService>();
 
 //DB connection:
 var connection = builder.Configuration.GetConnectionString("DefaultConnection");
